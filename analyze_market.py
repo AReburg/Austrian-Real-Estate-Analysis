4c6522b519e4b6b38fe00761444abb213e448d83
AReburg <arlin.gruber@gmail.com>
2022-06-07 23:58:05 +0200

    It is possilbe to select state or county resolution over price

--- analyze_market.py
+++ analyze_market.py
@@ -23,73 +23,124 @@ https://de.wikipedia.org/wiki/Amtlicher_Gemeindeschl%C3%BCssel
 gemeinde liste: https://secure.umweltbundesamt.at/edm_portal/redaList.do;jsessionid=59B9C7B2CFD801671BD5A24791259EB0.edmportalnode01?seqCode=cwzqtv5r84nvpr&entireLsq=true
 """
 
+import numpy as np
+
 
 def gkz(a):
     try:
-        pattern = re.compile(r"(?<!\d)\d{4,7}(?!\d)")
-        plz = pattern.findall(a)[0]
-        return plz
+        arr = np.where(a==df_lookup['plz'], a, 999)
+        return arr
     except IndexError:
         return None
 
 
-def get_geo_data(selector):
+def get_geo_data(selector, source='online'):
     """ Load Austrian geojson
+     counties["features"][0]['properties']['iso']
     https://github.com/ginseng666/GeoJSON-TopoJSON-Austria/blob/master/2021/simplified-99.9/laender_999_geo.json """
     if selector == 'gemeinden':
-        link = 'https://raw.githubusercontent.com/ginseng666/GeoJSON-TopoJSON-Austria/master/2021/simplified-99.5/gemeinden_995_geo.json'
+        if source == 'online':
+            link = 'https://raw.githubusercontent.com/ginseng666/GeoJSON-TopoJSON-Austria/master/2021/simplified-99.5/gemeinden_995_geo.json'
+            with urlopen(link) as response:
+                counties = json.load(response)
+        else:
+            with open("./data/gemeinden_999_geo.json") as a:
+                counties = json.load(a)
     elif selector == 'bezirke':
-        link = 'https://raw.githubusercontent.com/ginseng666/GeoJSON-TopoJSON-Austria/master/2021/simplified-99.9/bezirke_999_geo.json'
+        if source == 'online':
+            link = 'https://raw.githubusercontent.com/ginseng666/GeoJSON-TopoJSON-Austria/master/2021/simplified-99.9/bezirke_999_geo.json'
+            with urlopen(link) as response:
+                counties = json.load(response)
+        else:
+            with open("./data/bezirke_999_geo.json") as a:
+                counties = json.load(a)
     elif selector == 'laender':
-        link = 'https://raw.githubusercontent.com/ginseng666/GeoJSON-TopoJSON-Austria/master/2021/simplified-99.9/laender_999_geo.json'
+        if source == 'online':
+            link = 'https://raw.githubusercontent.com/ginseng666/GeoJSON-TopoJSON-Austria/master/2021/simplified-99.9/laender_999_geo.json'
+            with urlopen(link) as response:
+                counties = json.load(response)
+        else:
+            with open("./data/laender_999_geo.json") as a:
+                counties = json.load(a)
 
-    with urlopen(link) as response:
-        counties = json.load(response)
     return counties
 
 
 def load_data():
-    con = sqlite3.connect("./db/real_estate2.sqlite")
+    # load lookup data
+    df_lookup = pd.read_csv(r'laender_lookup.csv', sep=';')
+    df_lookup_l = pd.read_csv(r'counties_lookup.csv', sep=';')
+
+    # Load data from db
+    con = sqlite3.connect("./db/real_estate.sqlite")
     df = pd.read_sql_query("SELECT * FROM RealEstate", con)
     con.close()
-    df_lookup = pd.read_csv (r'laender_lookup.csv', sep = ';')
-    print(df_lookup.cc)
-    df_new = df
 
-    df_new['plz']  = df['location'].apply(gkz)
-
-
-    df_new['plz'] = df_new.plz.astype("int64")
+    # Set dtypes of the dataframe
+    df['Type'] = df['Type'].astype('category', errors='ignore')
+    df['plz'] = df.plz.fillna(0).astype("int64", errors='ignore')
+    # print(df['count'].head())
     df_lookup['plz'] = df_lookup.plz.astype("int64")
-    df_new= df_new.merge(df_lookup['cc'], how='outer', left_on='plz', right_on='cc')
-    df_new['cc'] = df_new['cc'].fillna(0).astype('int', errors='ignore')
-    df_new['cc'] = df_new['cc'].astype('str', errors='ignore')
-    df_new['price'] = df_new['price'].fillna(0).astype('int', errors='ignore')
-    plz_dat = pd.read_csv (r'plz.csv', sep = ';')
+    df_lookup['cc'] = df_lookup.cc.astype("int64")
+    df_lookup_l['county'] = df_lookup_l.bundesland.astype("category")
+    df_lookup_l['plz'] = df_lookup_l.plz.astype("int64")
+    df['price'] = df['price'].fillna(0)#.astype('int', errors='ignore')
+    df['price'] = pd.to_numeric(df['price'], errors='coerce')
 
-    # print(plz_dat)
-    #df_new= df_new.merge(plz_dat, how='outer', left_on='plz', right_on=['bezirk', 'bundesland'])
+    # merge dataframem with lookup table
+    df = df.merge(df_lookup[['plz','cc']], on=['plz'])
+    df = df.merge(df_lookup_l[['plz','county']], on=['plz'])
+    #df_new['cc'] = df_new['cc'].fillna(0).astype('int', errors='ignore')
+    #df_new['cc'] = df_new['cc'].astype('str', errors='ignore')
+    # df = df.groupby('cc', as_index=False)
+    #df.price = df.price.mean()
+    #print(df['plz'])
+    # print(df.price)
+    return df
 
-    #df_new.groupby(df_new['cc']).mean()
-    #print(df_new.head())
-    #print(df_new['cc'])
-    return df_new
 
-def make_graph(df, geo_data):
-    fig = px.choropleth_mapbox(df, geojson=geo_data, locations="cc",
-                               featureidkey="properties.iso", color="price",
+def make_graph(df, geo_data, sel='gemeinden'):
+    feat_key = ''
+    locations = ''
+    if sel == 'gemeinden':
+        feat_key = "properties.iso"
+        locations = "cc"
+    elif sel == 'laender':
+        feat_key = "properties.name"
+        locations = "county"
+    fig = px.choropleth_mapbox(df, geojson=geo_data, locations=locations,
+                               featureidkey=feat_key, color="price",
+                               #hover_name = 'plz',
+                               hover_data = ['price'], #'county' 'cc',
                                color_continuous_scale="Viridis",
-                               range_color=(0, df['price'].max),
+                               range_color=(df['price'].mean()*(1-0.3), df['price'].mean()*(1+0.3)),
                                mapbox_style="carto-positron",
-                               zoom=2, center = {"lat": 48.208176, "lon": 16.373819},
-                               opacity=0.1
-                               #labels={'price':'price'}
+                               zoom=6, center = {"lat": 47.809490, "lon": 13.055010},
+                               opacity=0.25,
+                               labels={'price':'price'}
                               )
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_layout(margin={"r":10,"t":30,"l":40,"b":10})
     fig.show()
 
 
-df = load_data()
-geo_data = get_geo_data('gemeinden') # bezirke, laender
-# make_graph(df, geo_data)
+def manipulate_data(df, sel='gemeinden'):
+    # print(f"{df['price'].mean()}")
+    #f['plz'] = df['plz'].astype('category', errors='ignore')
+    #df['count'] = df['cc'].value_counts()
+    #print(df['count'])
+    df2 = df.copy()
+    #print(df2.columns)
+    # print(df.groupby(['cc'], as_index=False).groups.keys())
+    # print(df.groupby(['cc'], as_index=False)['price'].mean())
+    if sel == 'gemeinden':
+        df2 = df2.groupby(['cc'], as_index=False).agg({'price': 'mean', 'plz': 'first', 'county': 'first'})
+    elif sel == 'laender':
+        df2 = df2.groupby(['county'], as_index=False).agg({'price': 'mean', 'plz': 'first'})
+    df2['price'] = df2['price'].round(0)
+    return df2
 
+df = load_data()
+res = 'laender'
+df2 = manipulate_data(df, res)
+geo_data = get_geo_data(res, source='offline') # bezirke, laender
+make_graph(df2, geo_data, res)
